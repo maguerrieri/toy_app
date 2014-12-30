92234823d881f4e93a292dc181151620e11cf62c
Mario Guerrieri <mguerrieri@outlook.com>
2014-12-29 17:42:50 -0800

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	def test
+		render text: "If you can see this, then something's working"
+	end
 end
